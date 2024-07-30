681c7d0f6c4c4a72905e80a16d33a000bdfa03ce
Daniel N. Lang <daniel.lang@velaia.de>
2024-07-30 14:04:07 +0200

    added pyproject and code for simple page

---
+++ src/fastht_ml_fungi/__init__.py
@@ -0,0 +1,24 @@
+from fasthtml.common import *
+from icecream import ic
+
+
+app = FastHTML()
+
+count = 0
+
+@app.get("/")
+def home():
+    return Title("Count Demo"), Main(
+        H1("Count Demo"),
+        P(f"Count is set to {count}", id="count"),
+        Button("Increment", hx_post="/increment", hx_target="#count", hx_swap="innerHTML")
+    )
+
+@app.post("/increment")
+def increment():
+    global count
+    ic(count)
+    count += 1
+    return f"Count is set to {count}"
+
+serve()
