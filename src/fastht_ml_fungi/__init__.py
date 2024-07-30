5b2d29ffc613c04208eb0f229bca5b79597bf4f6
Daniel N. Lang <daniel.lang@velaia.de>
2024-07-30 21:49:43 +0200

    formatting

--- src/fastht_ml_fungi/__init__.py
+++ src/fastht_ml_fungi/__init__.py
@@ -12,7 +12,8 @@ setup_toasts(app)
 @app.get("/")
 def home():
     return Title("Mushroom 🍄 Map"), Main(
-        H1("Mushroom 🍄 Map"), P("The map displays your mushroom observations."),
+        H1("Mushroom 🍄 Map"),
+        P("The map displays your mushroom observations."),
         A("Click to add new observation", href="/new-observation"),
     )
 
@@ -22,23 +23,29 @@ def new_observation():
     # return a Title and page for new observation that includes an image upload
     return Title("New Observation"), Main(
         H1("Add a New Observation"),
-        P("Please select the mushroom image for the observation. The photo should come with the GPS location where it has been taken in the metadata."),
+        P(
+            "Please select the mushroom image for the observation. The photo should come with the GPS location where it has been taken in the metadata."
+        ),
         Form(
-            Input(id="photo", type="file", name="photo"), Button("Submit", type="submit"),
-            action="/new-observation", method='POST', enctype="multipart/form-data", accept='image/*'
-        )
+            Input(id="photo", type="file", name="photo"),
+            Button("Submit", type="submit"),
+            action="/new-observation",
+            method="POST",
+            enctype="multipart/form-data",
+            accept="image/*",
+        ),
     )
 
+
 @app.post("/new-observation")
-async def new_observation_post(session, photo:str):
-    add_toast(session, "Photo upload successful", 'success')
+async def new_observation_post(session, photo: str):
+    add_toast(session, "Photo upload successful", "success")
     photo_content = await photo.read()
-    with open('./static/photo-output.jpg', 'wb') as output:
+    with open("./static/photo-output.jpg", "wb") as output:
         output.write(photo_content)
     ic(type(photo))
     return Title("Successful Submission "), Main(
-        H1("Information about Uploaded Photo"),
-        Img(src="/photo-output.jpg")
+        H1("Information about Uploaded Photo"), Img(src="/photo-output.jpg")
     )
 
 
